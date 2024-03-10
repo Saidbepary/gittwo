d229e90b3b9d37b5af583249858aeaf762c5c4ab
Said bepary <Shadk3130@gmail.com>
2024-03-10 13:02:03 +0600

    add new

--- local/node.js
+++ local/node.js
@@ -3,10 +3,44 @@ let sum=(a,b)=>a+b
 console.log(sum(3,5))
 let a=29, b=55
 let obj={
-    a:a,
+    a,
     b,
     print(){
         console.log(this)
     }
 }
-obj.print()
+//destructuring
+let {n,m,print}=obj
+console.log(a)
+let person={
+    name:'said',
+  age:20,
+  adress:{
+    city:'cp'
+
+
+  }
+}
+let {name,age,adress,adress:{city}}=person
+console.log(name,age,city)
+let arr=[28,48,55,66]
+let [first,second ,last]=arr
+console.log(first,second,last)
+//array to object
+let objarr=[
+    ['a',28],
+    ['b',4],
+    ['c',55],
+    ['d',66]
+]
+console.log(Object.fromEntries(objarr))
+//object to array
+console.log(Object.entries(person))
+//symbol
+let s1=Symbol()
+let s2=Symbol('test')
+console.log(s1==s2)
+let toss={
+    Head:Symbol('head'),
+    tail:Symbol('tail')
+}
